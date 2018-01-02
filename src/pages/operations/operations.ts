48a23e6e4a09ea550608956274f8d0f2f8370aad
Jordan Marques <jordanmarques1994@gmail.com>
2018-01-02 10:04:27 +0100

    rename tab home to operations

--- src/pages/operations/operations.ts
+++ src/pages/operations/operations.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: 'page-operations',
+  templateUrl: 'operations.html'
 })
-export class HomePage {
+export class OperationsPage {
 
   constructor(public navCtrl: NavController) {
 
