ef8c606bfb7ace3bfee280d06b5677d24a69a3a0
Jordan Marques <jordanmarques1994@gmail.com>
2018-01-03 16:00:02 +0100

    storage and working operation page

--- src/pages/operations/operations.ts
+++ src/pages/operations/operations.ts
@@ -1,27 +1,49 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-operations',
   templateUrl: 'operations.html'
 })
 export class OperationsPage {
-  private balance: number;
 
-  constructor(public navCtrl: NavController) {
-    this.balance = 300;
+  amount: number;
+  comment: string;
+  balance: number;
+
+  constructor(private storage: Storage) {
+    this.refreshBalance()
   }
 
   credit(amount: number) {
     if(!amount) return;
 
-    console.log(amount)
+    amount = Number(amount);
+    this.storage.set("balance",  this.balance + amount)
+                .then(() => {
+                  this.clear();
+                  this.refreshBalance()
+                })
   }
 
   debit(amount: number) {
     if(!amount) return;
 
-    console.log(amount)
+    amount = Number(amount);
+    this.storage.set("balance", this.balance - amount )
+                .then( () => {
+                  this.clear();
+                  this.refreshBalance()
+                });
   }
 
+  refreshBalance() {
+    this.storage.get("balance")
+                .then(balance => { this.balance = balance ? balance : 0 });
+  }
+
+  clear() {
+    this.amount = null;
+    this.comment = null;
+  }
 }
