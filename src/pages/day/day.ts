c0d1909cfefaf62af44f40681fdeab93524c09a7
Jordan Marques <jordanmarques1994@gmail.com>
2018-01-09 08:43:15 +0100

    feat: day page

--- src/pages/day/day.ts
+++ src/pages/day/day.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {BalanceProvider} from "../../providers/balance/balance-provider";
 
 @Component({
   selector: 'page-day',
   templateUrl: 'day.html'
 })
-export class DayPage {
+export class DayPage{
 
-  constructor(public navCtrl: NavController) {
+  private dailySpend: number;
 
+  constructor(public balanceProvider: BalanceProvider) {}
+
+  ionViewWillEnter() {
+    this.balanceProvider.get().then(balance => {
+      const today = new Date();
+      const actualMonth: number = today.getMonth();
+      const actualDate: number = today.getDate();
+      const remainingDays: number = this.lastDayOfMonth(actualMonth) - actualDate;
+
+      this.dailySpend = Number(balance / remainingDays).toPrecision(3);
+    })
   }
 
+  private lastDayOfMonth(month: number): number {
+  return new Date(new Date().getFullYear(), month, 0).getDate();
+}
 }
